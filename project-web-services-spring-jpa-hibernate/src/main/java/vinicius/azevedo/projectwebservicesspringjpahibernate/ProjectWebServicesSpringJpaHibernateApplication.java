bab772b033274ee91c7b06f5a9b20871b9243300
Vinícius Gabriel Azevedo <vinicius.azevedo2422@gmail.com>
2023-11-21 21:17:27 -0300

    Project created

---
+++ project-web-services-spring-jpa-hibernate/src/main/java/vinicius/azevedo/projectwebservicesspringjpahibernate/ProjectWebServicesSpringJpaHibernateApplication.java
@@ -0,0 +1,14 @@
+package vinicius.azevedo.projectwebservicesspringjpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ProjectWebServicesSpringJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWebServicesSpringJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ project-web-services-spring-jpa-hibernate/src/test/java/vinicius/azevedo/projectwebservicesspringjpahibernate/ProjectWebServicesSpringJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package vinicius.azevedo.projectwebservicesspringjpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWebServicesSpringJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
